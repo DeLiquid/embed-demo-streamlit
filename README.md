6eddf23a7150dcadb69fdff583dad50cd17b850d
Mark (Kailing) Ding <49173589+kailingding@users.noreply.github.com>
2024-06-12 17:41:09 -0700

    demo

--- README.md
+++ README.md
@@ -2,16 +2,4 @@
 
 In this repository, we will show you how to embed Brewit's chat agent into your Streamlit app.
 
-## Installation
-
-1. Install the required packages by running the following command:
-
-   ```bash
-   pip install -r requirements.txt
-   ```
-
-2. Run the Streamlit app by running the following command:
-
-   ```bash
-   streamlit run app.py
-   ```
+![Embed Demo](/img/brewit-embed-demo.png)
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 # load Brewit API key from .env file
 load_dotenv()
+BREWIT_API_KEY = os.getenv("BREWIT_API_KEY")
 
 brewit_url = "https://api.brewit.ai/v1/auth/signin_external"
 
@@ -21,32 +22,24 @@ def generate_jwt():
     }
 
     headers = {
-        "Content-Type": "application",
-        "Authorization": f"Bearer {os.getenv('BREWIT_API_KEY')}",
+        "Content-Type": "application/json",
+        "Authorization": BREWIT_API_KEY,
     }
 
     response = requests.request("POST", brewit_url, json=payload, headers=headers)
-
     if response.status_code == 200:
         return response.json()["token"]
     return None
 
 
+# Generate a JWT token
 jwt = generate_jwt()
+
+# Construct the Brewit chat agent embed URL
 embed_url = f"https://app.brewit.ai/embed/chat?workspace_id=3697a93c-daa4-4068-ad42-164a1b149710&resource_id=2c38be6d-2199-4297-a653-1053e2c9e5b6&jwt={jwt}"
 
-
+st.set_page_config(layout="wide")
 st.title("Brewit Embedded Chat Agent")
 
-
-components.iframe(
-    f"""<iframe
-  src="{embed_url}"
-  style={{
-    width: '100%',
-    height: '100%',
-    minHeight: '500px',
-    minWidth: '400px',
-  }}
-/>"""
-)
+# Embed the Brewit chat agent using an iframe
+components.iframe(embed_url, height=800, width=1300, scrolling=True)
